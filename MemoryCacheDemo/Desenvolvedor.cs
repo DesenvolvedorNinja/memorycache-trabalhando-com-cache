a6e699b44a340e3c75357997a13258d03397bae7
Romulo Cianci <rcianci@gmail.com>
2015-10-18 23:21:11 -0200

    Código-fonte do exemplo sobre MemoryCache publicado no blog http://desenvolvedor.ninja

---
+++ MemoryCacheDemo/Desenvolvedor.cs
@@ -0,0 +1,11 @@
+﻿namespace MemoryCacheDemo
+{
+    public class Desenvolvedor
+    {
+        public string Nome { get; set; }
+        public string Linguagem { get; set; }
+        public int AnosExperiencia { get; set; }
+
+        public override string ToString() => $"Nome: {Nome} / Linguagem: {Linguagem} / Anos Experiência: {AnosExperiencia}";
+    }
+}
---
+++ MemoryCacheTest/UnitTest1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.Caching;
+using MemoryCacheDemo;
+using System.Threading;
+
+namespace MemoryCacheTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private MemoryCache memoryCache;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            memoryCache = MemoryCache.Default;
+        }
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            Desenvolvedor dev = new Desenvolvedor();
+            dev.Nome = "Ninja";
+            dev.Linguagem = "C#";
+            dev.AnosExperiencia = 15;
+
+            CacheItemPolicy policy = new CacheItemPolicy();
+            policy.AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(60);
+
+            Assert.IsTrue(memoryCache.Add("dev", dev, policy));
+        }
+
+        [TestMethod]
+        public void TestGet()
+        {
+            Desenvolvedor dev = new Desenvolvedor();
+            dev.Nome = "Ninja";
+            dev.Linguagem = "C#";
+            dev.AnosExperiencia = 15;
+
+            CacheItemPolicy policy = new CacheItemPolicy();
+            policy.AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(60);
+
+            memoryCache.Add("devGet", dev, policy);
+
+            Desenvolvedor devGet = (Desenvolvedor)memoryCache["devGet"];
+            
+            Assert.AreEqual(dev.ToString(), devGet.ToString());
+        }
+
+        [TestMethod]
+        public void TestGetExpired()
+        {
+            Desenvolvedor dev = new Desenvolvedor();
+            dev.Nome = "Ninja";
+            dev.Linguagem = "C#";
+            dev.AnosExperiencia = 15;
+
+            CacheItemPolicy policy = new CacheItemPolicy();
+            policy.AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(1);
+
+            memoryCache.Add("devExpired", dev, policy);
+            Thread.Sleep(3000);
+
+            Desenvolvedor devGet = (Desenvolvedor)memoryCache["devExpired"];
+            
+            Assert.IsNull(devGet);
+        }
+
+        [TestMethod]
+        public void TestSliding()
+        {
+            Desenvolvedor dev = new Desenvolvedor();
+            dev.Nome = "Ninja";
+            dev.Linguagem = "C#";
+            dev.AnosExperiencia = 15;
+
+            CacheItemPolicy policy = new CacheItemPolicy();
+            policy.SlidingExpiration = TimeSpan.FromSeconds(3);
+
+            memoryCache.Add("devSliding", dev, policy);
+
+            Thread.Sleep(2000);
+            Desenvolvedor devGet = (Desenvolvedor)memoryCache["devSliding"];
+            Assert.AreEqual(dev.ToString(), devGet.ToString());
+
+            Thread.Sleep(2000);
+            devGet = (Desenvolvedor)memoryCache["devSliding"];
+            Assert.AreEqual(dev.ToString(), devGet.ToString());
+
+            Thread.Sleep(4000);
+            devGet = (Desenvolvedor)memoryCache["devSliding"];
+            Assert.IsNull(devGet);
+        }
+
+        [TestMethod]
+        public void TestRemove()
+        {
+            Desenvolvedor dev = new Desenvolvedor();
+            dev.Nome = "Ninja";
+            dev.Linguagem = "C#";
+            dev.AnosExperiencia = 15;
+
+            CacheItemPolicy policy = new CacheItemPolicy();
+            policy.AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(60);
+            
+            memoryCache.Add("devRemove", dev, policy);
+
+            Desenvolvedor devGet = (Desenvolvedor)memoryCache["devRemove"];
+            Assert.AreEqual(dev.ToString(), devGet.ToString());
+
+            memoryCache.Remove("devRemove");
+
+            devGet = (Desenvolvedor)memoryCache["devRemove"];
+            Assert.IsNull(devGet);
+        }
+
+        [TestMethod]
+        public void TestCount()
+        {
+            MemoryCache memoryCount = new MemoryCache("count");
+
+            Desenvolvedor dev = new Desenvolvedor();
+            dev.Nome = "Ninja";
+            dev.Linguagem = "C#";
+            dev.AnosExperiencia = 15;
+
+            CacheItemPolicy policy = new CacheItemPolicy();
+            policy.AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(2);
+
+            memoryCount.Add("devRemove", dev, policy);
+
+            Assert.AreEqual(1, memoryCount.GetCount());
+        }
+    }
+}
